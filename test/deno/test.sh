b88b7f3afbcaf612e85f33cf93c5d334dbafc813
Michael Tyson <myty@users.noreply.github.com>
2024-01-28 21:06:21 +0000

    Update Deno version options in devcontainer-template.json

---
+++ test/deno/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $(dirname "$0")
+source test-utils.sh
+
+# Template specific tests
+check "distro" lsb_release -c
+
+# Image specific tests
+check "deno" deno --version
+
+# Report result
+reportResults
