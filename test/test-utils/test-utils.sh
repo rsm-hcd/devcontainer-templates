515857644bcf1d78eea4c50d65c51fe0f119562d
Michael Tyson <myty@users.noreply.github.com>
2024-01-31 20:24:48 +0000

    Add dependabot, test utility functions

--- test/test-utils/test-utils.sh
+++ test/test-utils/test-utils.sh
@@ -30,3 +30,42 @@ reportResults() {
         exit 0
     fi
 }
+
+# Useful for scenarios where UID/GID is not automatically updated - happens in GitHub Actions w/Docker Compose
+fixTestProjectFolderPrivs() {
+    if [ "${USERNAME}" != "root" ]; then
+        TEST_PROJECT_FOLDER="${1:-$SCRIPT_FOLDER}"
+        FOLDER_USER="$(stat -c '%U' "${TEST_PROJECT_FOLDER}")"
+        if [ "${FOLDER_USER}" != "${USERNAME}" ]; then
+            echoStderr "WARNING: Test project folder is owned by ${FOLDER_USER}. Updating to ${USERNAME}."
+            sudo chown -R ${USERNAME} "${TEST_PROJECT_FOLDER}"
+        fi
+    fi
+}
+
+checkExtension() {
+    # Happens asynchronusly, so keep retrying 10 times with an increasing delay
+    EXTN_ID="$1"
+    TIMEOUT_SECONDS="${2:-10}"
+    RETRY_COUNT=0
+    echo -e -n "\n🧪 Looking for extension $1 for maximum of ${TIMEOUT_SECONDS}s"
+    until [ "${RETRY_COUNT}" -eq "${TIMEOUT_SECONDS}" ] || \
+        [ ! -e $HOME/.vscode-server/extensions/${EXTN_ID}* ] || \
+        [ ! -e $HOME/.vscode-server-insiders/extensions/${EXTN_ID}* ] || \
+        [ ! -e $HOME/.vscode-test-server/extensions/${EXTN_ID}* ] || \
+        [ ! -e $HOME/.vscode-remote/extensions/${EXTN_ID}* ]
+    do
+        sleep 1s
+        (( RETRY_COUNT++ ))
+        echo -n "."
+    done
+
+    if [ ${RETRY_COUNT} -lt ${TIMEOUT_SECONDS} ]; then
+        echo -e "\n✅ Passed!"
+        return 0
+    else
+        echoStderr -e "\n❌ Extension $EXTN_ID not found."
+        FAILED+=("$LABEL")
+        return 1
+    fi
+}
