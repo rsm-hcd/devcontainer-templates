f3e3f6e5e0da60ad314563557a2f1d5357858015
Michael Tyson <myty@users.noreply.github.com>
2024-03-18 01:50:18 +0000

    upgrade deno to v1.41.3

--- test/deno/main_test.ts
+++ test/deno/main_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.213.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.220.1/assert/mod.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
